3609059fcc62e76c666d2034b58921a38044b0c4
brendsupport <79204947+brendsupport@users.noreply.github.com>
2021-11-04 15:22:23 +0400

    Hazır

--- brend_installer/language.py
+++ brend_installer/language.py
@@ -5,25 +5,21 @@ from . import logo, console, bilgi
 def importlang ():
     console.clear()
     logo()
-    bilgi("\n\[1] Türkçe\n\[2] Azərbaycanca\n\[3] English\n\[4] O'zbek")
-    Dil = Prompt.ask("[bold yellow]Zəhmət olmasa 1 dil seçin / Please select a language[/]", choices=["1", "2", "3", "4"], default="1")
+    bilgi("\n\[1] Azərbaycanca\n\[2] Türkcə\n\[3] English")
+    Dil = Prompt.ask("[bold yellow]Zəhmət olmasa 1 dil seçin / Please select a language[/]", choices=["1", "2", "3"], default="1")
 
     if Dil == "1":
+        COUNTRY = "Azerbaijan"
+        LANGUAGE = "Az"
+        TZ = "Asia/Baku"
+    elif Dil == "2":
         COUNTRY = "Turkey"
         LANGUAGE = "TR"
         TZ = "Europe/Istanbul"
-    elif Dil == "2":
-        COUNTRY = "Azerbaijan"
-        LANGUAGE = "AZ"
-        TZ = "Asia/Baku"
     elif Dil == "3":
         COUNTRY = "United Kingdom"
         LANGUAGE = "EN"
         TZ = "Europe/London"
-    elif Dil == "4":
-        COUNTRY = "Uzbekistan"
-        LANGUAGE = "UZ"
-        TZ = "Asia/Tashkent"
 
     return COUNTRY, LANGUAGE, TZ
 
